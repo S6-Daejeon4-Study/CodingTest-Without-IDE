40321bdfac3b358889ef00eaefc214f78d552d84
comkkyu <bakha1105@gmail.com>
2021-11-19 11:51:41 +0900

    1119 한선규

---
+++ 3주차/12금_L1_크레인인형뽑기게임/Solution_한선규.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<Integer>();
+        
+        for(int i = 0; i < moves.length; i++) {
+            for(int j = 0; j < board.length; j++) {          
+                if(board[j][moves[i] - 1] != 0) {
+                    if(stack.empty()) {
+                        stack.push(board[j][moves[i] - 1]);
+                    } else {
+                        if(board[j][moves[i] - 1] == stack.peek()) {
+                            stack.pop();
+                            answer += 2;
+                        } else {
+                            stack.push(board[j][moves[i] - 1]);
+                        }
+                    }
+                    
+                    board[j][moves[i] - 1] = 0;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
