4270997be8ff9914979f5c3a3d9b63523a368e06
WooJeongYeon <wjygogogo@gmail.com>
2021-12-04 23:07:18 +0900

    1204 우정연

---
+++ 4주차/16토_L1_두정수사이의합/Solution_우정연.java
@@ -0,0 +1,15 @@
+
+public class PRO12912_두정수사이의합 {
+	class Solution {
+	    public long solution(int a, int b) {
+	        long answer = 0;
+	        long max = Math.max(a, b);
+	        long min = Math.min(a, b);
+	        answer = (max + min) * ((max - min + 1) / 2);
+	        if((max - min + 1) % 2 == 1) {
+	            answer += (max + min) / 2;
+	        }
+	        return answer;
+	    }
+	}
+}
