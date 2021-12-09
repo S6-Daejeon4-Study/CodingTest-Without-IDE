43e7ca42824bee30882519a93982b280cf4fde7e
김유진 <determination9212@gmail.com>
2021-12-10 01:09:02 +0900

    1210 김유진 8.3/100

---
+++ 5주차/06월_L3_금과 은 운반하기/Solution_김유진.java
@@ -0,0 +1,87 @@
+import java.util.PriorityQueue;
+
+public class P86053_금과은운반하기2 {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 10;
+		int[] g = { 100 };
+		int[] s = { 100 };
+		int[] w = { 7 };
+		int[] t = { 10 };
+
+		System.out.println(solution(a, b, g, s, w, t));
+	}
+
+	static public long solution(int a, int b, int[] g, int[] s, int[] w, int[] t) {
+		return calcTime(a, b, g, s, w, t);
+	}
+
+	static long calcTime(int a, int b, int[] g, int[] s, int[] w, int[] t) {
+		PriorityQueue<Truck> pq = new PriorityQueue<>();
+		int len = w.length;
+		for (int i = 0; i < len; i++) {
+			pq.add(new Truck(i, t[i]));
+		}
+		long answer = 0;
+
+		while (a > 0 && b > 0) {
+			Truck truck = pq.poll();
+			int idx = truck.i;
+			double nowa = a / (double) (a + b);
+			int tmpGold = (int) (w[idx] * nowa);
+			int tmpSilver = w[idx] - tmpGold;
+
+			if (g[idx] >= tmpGold && s[idx] >= tmpSilver) {
+				a -= tmpGold;
+				b -= tmpSilver;
+				g[idx] -= tmpGold;
+				s[idx] -= tmpSilver;
+				answer = truck.t;
+				pq.add(new Truck(idx, answer + (long) (2 * t[idx])));
+			} else if (g[idx] >= tmpGold && s[idx] < tmpSilver) {
+				tmpSilver = s[idx] >= 0 ? s[idx] : 0;
+				tmpGold = w[idx] - tmpSilver >= g[idx] ? g[idx] : w[idx] - tmpSilver;
+				a -= tmpGold;
+				b -= tmpSilver;
+				g[idx] -= tmpGold;
+				s[idx] -= tmpSilver;
+				answer = truck.t;
+				pq.add(new Truck(idx, answer + (long) (2 * t[idx])));
+			} else if (g[idx] < tmpGold && s[idx] >= tmpSilver) {
+				tmpGold = g[idx] >= 0 ? g[idx] : 0;
+				tmpSilver = w[idx] - tmpGold >= s[idx] ? s[idx] : w[idx] - tmpGold;
+				a -= tmpGold;
+				b -= tmpSilver;
+				g[idx] -= tmpGold;
+				s[idx] -= tmpSilver;
+				answer = truck.t;
+				pq.add(new Truck(idx, answer + (long) (2 * t[idx])));
+			} else {
+				tmpGold = g[idx] < 0 ? 0 : g[idx];
+				tmpSilver = s[idx] < 0 ? 0 : s[idx];
+				a -= tmpGold;
+				b -= tmpSilver;
+				g[idx] -= tmpGold;
+				s[idx] -= tmpSilver;
+				answer = truck.t;
+			}
+
+		}
+		return answer;
+	}
+
+	static class Truck implements Comparable<Truck> {
+		int i;
+		long t;
+
+		public Truck(int idx, long time) {
+			this.i = idx;
+			this.t = time;
+		}
+
+		@Override
+		public int compareTo(Truck o) {
+			return Long.compare(this.t, o.t);
+		}
+	}
+}
