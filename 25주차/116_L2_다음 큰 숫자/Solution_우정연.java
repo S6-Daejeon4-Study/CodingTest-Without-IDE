9d3345540ac1817426374a0e92d0c7053c4a186d
WooJeongYeon <wjygogogo@gmail.com>
2022-05-18 06:57:21 +0900

    0518

---
+++ 25주차/116_L2_다음 큰 숫자/Solution_우정연.java
@@ -0,0 +1,27 @@
+// Integer.bitCount() 라는게 있네..!
+class Solution {
+    int max;
+    public int solution(int n) {
+        int answer = 0;
+        max = Integer.MAX_VALUE;
+        max = getOneCnt(n);
+        for(int i = n + 1 ; i <= 1000000 ; i++) {
+            if(getOneCnt(i) == max) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+    public int getOneCnt(int n) {
+        int cnt = 0;
+        while(n != 0) {
+            if((n & 1) == 1)
+                cnt++;
+            if(cnt > max)
+                return -1;
+            n >>= 1;
+        }
+        return cnt;
+    }
+}
